d7fd83790a9fbabeaa927c45f49b2130721f1c54
Shaoyang <dwight.zhang@mail.utoronto.ca>
2021-02-04 23:23:11 -0500

    Modified m1.cpp by implementing 'findDistanceBetweenTwoPoints' fuction.

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -21,8 +21,9 @@
 #include <iostream>
 #include "m1.h"
 #include "StreetsDatabaseAPI.h"
+#include <math.h>
 
-
+using namespace std;
 // loadMap will be called with the name of the file that stores the "layer-2"
 // map data accessed through StreetsDatabaseAPI: the street and intersection 
 // data that is higher-level than the raw OSM data). 
@@ -57,3 +58,67 @@ void closeMap() {
     //Clean-up your map related data structures here
     
 }
+
+
+// Returns the distance between two (latitude,longitude) coordinates in meters
+// Speed Requirement --> moderate
+double findDistanceBetweenTwoPoints(std::pair<LatLon, LatLon> points){
+    double distanceBetweenTwoPoints=0;
+    double latInRadius1,lonInRadius1,latInRadius2,lonInRadius2;
+    //converting longitude and latitude from degree to radius
+    latInRadius1=points.first.latitude()*kDegreeToRadian;
+    lonInRadius1=points.first.longitude()*kDegreeToRadian;
+    latInRadius2=points.second.latitude()*kDegreeToRadian;
+    lonInRadius2=points.second.longitude()*kDegreeToRadian;
+    //convert to position to (x,y) in meters
+    double x1, y1, x2, y2;
+    x1=lonInRadius1*cos((latInRadius2+latInRadius1)/2);
+    y1=latInRadius1;
+    x2=lonInRadius2*cos((latInRadius2+latInRadius1)/2);
+    y2=latInRadius2;
+    //using Pythagora's theorem to calculate distance between two points
+    distanceBetweenTwoPoints=kEarthRadiusInMeters*sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    return distanceBetweenTwoPoints;
+}
+
+/*********************below functions are not properly implemented, only used to testing
+// Returns the length of the given street segment in meters
+// Speed Requirement --> moderate
+double findStreetSegmentLength(StreetSegmentIdx street_segment_id){
+    double streetSegmentLength;
+    
+    return streetSegmentLength;
+}
+
+
+// Returns the travel time to drive from one end of a street segment in 
+// to the other, in seconds, when driving at the speed limit
+// Note: (time = distance/speed_limit)
+// Speed Requirement --> high 
+double findStreetSegmentTravelTime(StreetSegmentIdx street_segment_id){
+    double streetSegmentTravelTime;
+    
+    return streetSegmentTravelTime;
+}
+
+// Returns the nearest intersection to the given position
+// Speed Requirement --> none
+IntersectionIdx findClosestIntersection(LatLon my_position){
+    IntersectionIdx closestIntersection;
+    
+    return closestIntersection;
+}
+
+
+///////////////////
+double findStreetLength(StreetIdx street_id){
+    double x;
+    return x;
+}
+
+double findFeatureArea(FeatureIdx feature_id){
+    double y;
+    return y;
+}
+ * 
+ * /
